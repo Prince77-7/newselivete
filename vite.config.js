f01a70e857235c1d9e010bcb2782bc015b22d5f7
Prince <prince@grey.pm>
2025-05-22 11:33:01 -0500

    allow domains

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	preview: {
+		allowedHosts: [
+			/^(.*\.)?wasaw\.com$/  // Allow all wasaw.com domains and subdomains
+		]
+	}
 }); 
