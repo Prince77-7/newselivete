ea133f2c2a765cb7aa0a43611d8453519048bc4a
Prince <prince@grey.pm>
2025-05-22 11:38:28 -0500

    other site allowing

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,19 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	server: {
+		host: '0.0.0.0',
+		strictPort: false,
+	},
 	preview: {
+		host: '0.0.0.0',
+		port: 4545,
+		strictPort: false,
 		allowedHosts: [
-			/^(.*\.)?wasaw\.com$/  // Allow all wasaw.com domains and subdomains
+			'localhost',
+			'test.wasaw.com',
+			'wasaw.com',
+			'.wasaw.com'  // This should match all subdomains
 		]
 	}
 }); 
